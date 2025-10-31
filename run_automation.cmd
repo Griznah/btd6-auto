8f635d573e1c57c93d26d3a70efafc9a94c4236f
Griznah <oms@griznah.net>
2025-10-31 22:58:20 +0100

    converting to uv from pip

--- run_automation.cmd
+++ run_automation.cmd
@@ -4,7 +4,7 @@ SET ENV_NAME=btd6env
 REM Create the virtual environment if it doesn't exist
 IF NOT EXIST "%ENV_NAME%\Scripts\activate.bat" (
     echo Creating virtual environment: %ENV_NAME%
-    python -m venv "%ENV_NAME%"
+    uv venv "%ENV_NAME%" --python 3.13
 )
 
 REM Activate the virtual environment
@@ -12,7 +12,7 @@ call "%ENV_NAME%\Scripts\activate.bat"
 
 REM Install dependencies from requirements.txt
 echo Installing dependencies...
-uv pip install -r requirements.txt -q
+uv sync
 
 REM Run the BTD6 automation bot
 echo Starting BTD6 Automation Bot...
